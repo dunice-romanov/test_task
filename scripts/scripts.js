77aeb0f20acf860bc1fcb119191d3639498d2ebd
dunice-aromanov <artem.romanov@dunice.ru>
2017-02-07 10:20:53 +0300

    added date function and some fixes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,30 +12,47 @@
 
     var str = JSON.stringify(localStData);
 
-    function makeRow(status, name, author, lastUpdated) {
-        
-        function makeRowInnerHtml(status, name, author) {
-            var htmlResult = "<td>" + status + "</td>" + "<td>" + name + "</td>" + "<td>" + author + "</td>" + "<td>" + new Date() + "</td>";
+    function makeRow(status, name, author, date) {
+        function makeRowInnerHtml(status, name, author, date) {
+            var htmlResult = "<td>" + status + "</td>" + 
+                "<td>" + name + "</td>" + "<td>" + author + "</td>" + "<td>" + getDateFormat(date) + "</td>";
             return htmlResult;
         }
         
         var element = document.createElement("tr");
-        element.innerHTML = makeRowInnerHtml(status, name, author, lastUpdated);
-        
+        element.innerHTML = makeRowInnerHtml(status, name, author, date);
         return element;
     }
 
     buttonAdd.onclick = function () {
         var author = $("#textAuthor").val(),
             name = $("#textTitle").val(),
-            status = document.getElementById('statusCheckBox').checked;
-        
-        var newRow = makeRow(status, name, author);
-        $("#table").append(newRow);
+            status = document.getElementById("checkboxStatus").checked;
+        var newRow = makeRow(status, name, author, new Date());
+        $("#tableTasks").append(newRow);
     };
 
 
     function inputText(context) {
         context.value = '';
     }
+    
+    function getDateFormat(date) {
+        var monthNames = [
+          "Январь", "Февраль", "Март", "Апрель", "Май",
+            "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
+        ];
+        
+        var day = date.getDay(), 
+            month = date.getMonth(),
+            year = date.getFullYear(),
+            hour = date.getHours(),
+            minute = date.getMinutes();
+        
+        var result = day + ':' + monthNames[month] + ':' + year  + '   ' + hour + ':' + minute;
+        
+        return result;
+    }
+    
+    
 })();
