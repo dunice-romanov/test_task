fb35fdd96723c4cb6b55d107d85bbea0a7f0d701
Romanov Artem <artem.romanov@dunice.ru>
2017-02-07 21:26:58 +0300

    placeholders and refresh stable

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,7 +6,10 @@
         ID_BUTTON_ADD = "buttonAdd",
         ID_DIV_INPUT = "divInput",
         ID_DIV_TABLE = "divTable",
-        ID_TABLE_TASKS = "tableTasks"
+        ID_TABLE_TASKS = "tableTasks",
+        KEY = 'key',
+        KEY_SEPARATOR = ',';
+        
         
     function CreateLocalStorageData(date, title, author) {
         this.id = +date;
@@ -16,6 +19,23 @@
         this.updated = date;
     }
 
+    /*
+        Возможно нужен поиск одинаковых ключей перед добавлением
+    */
+    function addKeyToLocalStorage(key) {
+        var keys = localStorage.getItem(KEY),
+            keysArray;
+        if (keys != null) {
+            keysArray = keys.split(KEY_SEPARATOR);
+            keysArray.push(key);
+            keys = keysArray.join(KEY_SEPARATOR);
+        } else {
+            keysArray = [ key ];
+            keys = keysArray.join(KEY_SEPARATOR);
+        };
+        
+        localStorage.setItem(KEY, keys);
+    }
     
     function addToLocalStorage(date, title, author) {
         
@@ -42,7 +62,6 @@
         var oldId = id, 
             oldLocalStorageObject = findObject(oldId),
             localStorageObjectUpdatedString;
-        
         if (oldLocalStorageObject[attribute] == value) {
             
             return false;
@@ -60,13 +79,6 @@
         
     }
     
-    function enableInput() {
-        alert('enabled');
-    }
-    
-    function disableInput(e) {
-        e.target.setAttribute('disabled', 'true');
-    }
     
     function makeRow(id, status, name, author, date) {
         
@@ -83,10 +95,36 @@
         var element = document.createElement("tr");
         element.setAttribute('id', +date);
         element.innerHTML = makeRowInnerHtml(id, status, name, author, date);
-        
+        $("#" + ID_TABLE_TASKS).append(element);
         return element;
     }
-
+    
+    function makeRowsFromKeys(keys) {
+        if (keys == null) 
+            return;
+        var keysArray = keys.split(KEY_SEPARATOR),
+            localStorageObjects = [],
+            storage = localStorage;
+        
+        for(var i = 0; i < keysArray.length; i++) {
+            var localStrorageObject = JSON.parse(storage.getItem(keysArray[i]));
+            
+            localStorageObjects.push(localStrorageObject);
+        }
+        for (var i = 0; i < localStorageObjects.length; i++) {
+            var id = localStorageObjects[i].id,
+                title = localStorageObjects[i].title,
+                author = localStorageObjects[i].author,
+                done = localStorageObjects[i].done,
+                updated = localStorageObjects[i].date;
+            
+            makeRow(id, done, title, author, new Date(id));
+        }
+        
+    }
+    
+    makeRowsFromKeys(localStorage.getItem(KEY));
+    
     /*
         clearing all inputTexts in arguments,
         argument is the jquery link!
@@ -124,21 +162,23 @@
         Event functions
     */
     buttonAdd.onclick = function () {
+ 
+        
         var author = $("#" + ID_AUTHOR).val(),
             title = $("#" + ID_TITLE).val(),
-            date = new Date();
+            date = new Date(),
+            id = +date;
         
         if (author == '' || title == '') 
             return;
-        var rr = +date;
         
-        var newRow = makeRow(+date, false, title, author, date);
+        var newRow = makeRow(id, false, title, author, date);
         
         addToLocalStorage(date, title, author);
+        addKeyToLocalStorage(id);
         
-        $("#" + ID_TABLE_TASKS).append(newRow);
         
-        clearInputTexts($('#textAuthor'), $('#textTitle'));
+        clearInputTexts($('#' + ID_AUTHOR), $('#' + ID_TITLE));
     };
     
     
